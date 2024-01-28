6ad466a097353f264a5873babe9de5a1d8eda5a8
paramessanjay <paramessanjay@gmail.com>
2024-01-28 19:39:18 +0530

    episode 1

---
+++ App.js
@@ -0,0 +1,14 @@
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createELement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I'm an h1 tag"),
+//     React.createElement("h2", {}, "I'm an h2 tag"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createELement("h1", {}, "I'm an h1 tag"),
+//     React.createElement("h2", {}, "I'm an h2 tag"),
+//   ]),
+// ]);
+// // JSX
+// console.log(parent); // object
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
